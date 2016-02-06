738fcc4caa2d96b04c3fdeb80692e7b029781283
XenoX <xenox1337@gmail.com>
2016-02-06 12:16:01 +0100

    Exercice 2 chap 4

--- 04/exo2/index.php
+++ 04/exo2/index.php
@@ -1,24 +1,7 @@
 <?php
+    if (isset($_POST)) { $word = translator($_POST['word']); }
+
     function translator($string) {
-        $tranlate = array(
-            'a' => 'y', 'b' => 'p', 'c' => 'l', 'd' => 't', 'e' => 'a', 'f' => 'v',
-            'g' => 'k', 'h' => 'r', 'i' => 'e', 'j' => 'z', 'k' => 'g', 'l' => 'm',
-            'm' => 's', 'n' => 'h', 'o' => 'u', 'p' => 'b', 'q' => 'x', 'r' => 'n',
-            's' => 'c', 't' => 'd', 'u' => 'i', 'v' => 'j', 'w' => 'f', 'x' => 'q',
-            'y' => 'o', 'z' => 'w'
-        );
-
-        $word = explode('.', $string);
-
-        foreach ($word as $key => $value) {
-            $word[$key] = $tranlate[$value];
-        }
-
-        return implode('.', $word);
-    }
-
-    // Exercise 4/4
-    function translatorWithoutDot($string) {
         $tranlate = array(
             'a' => 'y', 'b' => 'p', 'c' => 'l', 'd' => 't', 'e' => 'a', 'f' => 'v',
             'g' => 'k', 'h' => 'r', 'i' => 'e', 'j' => 'z', 'k' => 'g', 'l' => 'm',
@@ -44,7 +27,13 @@
     </head>
 
     <body>
-        <p>a.p.r.i.c.o.t = <?php echo(translator('a.p.r.i.c.o.t')); ?></p>
-        <p>apricot = <?php echo(translatorWithoutDot('apricot')); ?></p>
+        <p>
+            Word to translate :
+            <form action="" method="post">
+                <input type="text" id="word" name="word" />
+                <input type="submit" id="submit" name="submit" value="Envoyer" />
+            </form>
+        </p>
+        <p><?php if (isset($word)) { echo $word; } ?></p>
     </body>
 </html>
